4b3bff53563739bf62f046c8369cc28c141e4c2a
Farid Ureche <elturcofarid@gmail.com>
2024-11-27 21:16:04 +0100

    Modificación para ejecucion desde EKS

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:14.0-jdk-slim
+FROM openjdk:15.0-jdk-slim
 VOLUME /tmp
 EXPOSE 8090
 ADD ./target/zuul-server-0.0.1-SNAPSHOT.jar zuul-server.jar
--- src/main/java/com/co/solati/app/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/co/solati/app/zuul/oauth/ResourceServerConfig.java
@@ -35,12 +35,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/api/segurity/**").permitAll()
-		.antMatchers(HttpMethod.GET, "/api/productos/listar", "/api/items/listar", "/api/usuarios/usuarios").permitAll()
-		.antMatchers(HttpMethod.GET, "/api/productos/ver/{id}", 
-				"/api/items/ver/{id}/cantidad/{cantidad}", 
-				"/api/usuarios/usuarios/{id}").hasAnyRole("ADMIN", "USER")
-		.antMatchers("/api/productos/**", "/api/items/**", "/api/usuarios/**").hasRole("ADMIN")
+		http.authorizeRequests().antMatchers("/api/recordlogs/**","/api/auth2/**","/actuator/**").permitAll()
 		.anyRequest().authenticated()
 		.and().cors().configurationSource(corsConfigurationSource());
 	}
